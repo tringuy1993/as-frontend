356beb0410e435a5027d1f26864a9f32b0a594bd
Arbol <tringuy1993@gmail.com>
2023-06-10 12:23:32 -0700

    update Footer

--- components/Shell/Footer/Footer.tsx
+++ components/Shell/Footer/Footer.tsx
@@ -1,31 +1,48 @@
 //https://upmostly.com/tutorials/how-to-use-chart-js-with-react
 "use client";
+import { ActionIcon, Box, Divider, Text, Title } from "@mantine/core";
 import { IoMail, IoLogoLinkedin, IoLogoGithub } from "react-icons/io5";
-// import "./Footer.module.css";
+import { useStyles } from "./styles";
+import ModalComp from "@/components/modal/Modal";
+import { Contact } from "@/components/contact/Contact";
+import { useDisclosure } from "@mantine/hooks";
+
 const Footer = () => {
+  const { classes } = useStyles();
+  const [opened, { open, close }] = useDisclosure(false);
   return (
-    <footer id="contact">
-      <h2> Tri Nguyen &middot; Avid Learner </h2>
-      <ul>
-        <li>
-          <a href="https://www.linkedin.com/in/tringuyen-healthphysicist/">
-            <IoLogoLinkedin />
-          </a>
-        </li>
-        <li>
-          <a href="https://github.com/tringuy1993">
-            <IoLogoGithub />
-          </a>
-        </li>
-        <li>
-          <a href="mailto:tringuy1993@gmail.com">
-            <IoMail />
-            {/* <IoLogoGithub /> */}
-          </a>
-        </li>
-      </ul>
-      <a href="/disclaimers"> Disclaimers</a>
-    </footer>
+    <Box style={{ textAlign: "center" }}>
+      <Divider />
+      {/* <Title>Tri Nguyen &middot; Avid Learner</Title> */}
+      <Text>
+        <ul className={classes.list}>
+          <li>
+            <IoLogoLinkedin
+              onClick={() => {
+                window.location.href =
+                  "https://www.linkedin.com/in/tringuyen-healthphysicist/";
+              }}
+            />
+          </li>
+          <li>
+            {/* <a href="https://github.com/tringuy1993"> */}
+            <IoLogoGithub
+              onClick={() => {
+                window.location.href = "https://github.com/tringuy1993";
+              }}
+            />
+            {/* </a> */}
+          </li>
+          <li>
+            <IoMail onClick={open} />
+            <ModalComp opened={opened} open={open} close={close}>
+              <Contact />
+            </ModalComp>
+          </li>
+        </ul>
+      </Text>
+      <a href="/disclaimers">Disclaimers</a>
+    </Box>
   );
 };
 
