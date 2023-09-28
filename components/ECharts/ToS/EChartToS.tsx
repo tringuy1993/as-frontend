920e702ca77a58f9f6cf49e1f96f4cd44503bfb9
ArbolitoUbuntu <tringuy1993@gmail.com>
2023-09-27 20:59:09 -0700

    update Date for EChartToS

--- components/ECharts/ToS/EChartToS.tsx
+++ components/ECharts/ToS/EChartToS.tsx
@@ -1,9 +1,26 @@
 import React, { useState, useEffect } from "react";
 import { EChartToS_Opts } from "./EChartToS_Opts";
-import { Grid } from "@mantine/core";
+import { Card, Grid } from "@mantine/core";
 import { EChart_Opts_VolOI } from "../EChart_Opts_VolOI";
 import { modify_data } from "../UtilECharts";
 import { EChartThemed } from "../EChartThemed";
+
+function convertToPST(timestampStr) {
+  // Create a JavaScript Date object from the input string
+  const timestamp = new Date(timestampStr);
+
+  // Get the UTC offset for PST (Pacific Standard Time) in minutes
+  const pstOffsetMinutes = -480; // PST is UTC-8
+
+  // Calculate the PST timestamp by applying the UTC offset
+  const pstTimestamp = new Date(timestamp.getTime() + pstOffsetMinutes * 60000);
+
+  // Format the PST timestamp as a string with 'PST' at the end
+  const pstTimeStr = pstTimestamp.toISOString().replace('T', ' ').replace(/\.\d{3}Z$/, ' PST');
+
+  return pstTimeStr;
+}
+
 
 export default function EChartToS({ symbol, data, theoData, greek }) {
   let ecOptions, ecVoloptions;
@@ -19,16 +36,19 @@ export default function EChartToS({ symbol, data, theoData, greek }) {
   }
 
   return (
-    <Grid>
-      <Grid.Col sm={12} md={6}>
-        <EChartThemed option={{ ...ecOptions }} style={{ height: "650px" }} />
-      </Grid.Col>
-      <Grid.Col sm={12} md={6}>
-        <EChartThemed
-          option={{ ...ecVoloptions }}
-          style={{ height: "650px" }}
-        />
-      </Grid.Col>
-    </Grid>
+    <Card withBorder radius="md">
+      <Grid>
+        <Grid.Col sm={12} md={6}>
+          <EChartThemed option={{ ...ecOptions }} style={{ height: "650px" }} />
+        </Grid.Col>
+        <Grid.Col sm={12} md={6}>
+          <EChartThemed
+            option={{ ...ecVoloptions }}
+            style={{ height: "650px" }}
+          />
+        </Grid.Col>
+        <small>Last Updated: {convertToPST(data[0].saved_datetime)}</small>
+      </Grid>
+    </Card>
   );
 }
